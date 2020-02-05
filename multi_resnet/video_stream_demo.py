12e2e083c2030f671e9881d0faff52e43de271d4
baoanh1310 <dataholicguy@gmail.com>
2020-02-05 23:21:38 +0700

    Video stream demo for Multitask ResNet model

---
+++ multi_resnet/video_stream_demo.py
@@ -0,0 +1,115 @@
+# USAGE
+# python video_stream_demo.py --prototxt ../face_detector/deploy.prototxt.txt --model ../face_detector/res10_300x300_ssd_iter_140000.caffemodel
+
+# import the necessary packages
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import imutils
+import pickle
+import time
+import cv2
+import os
+import tensorflow as tf
+import model
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--prototxt", required=True,
+	help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,
+	help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.3,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+# load our serialized model from disk
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+
+# initialize the video stream and allow the cammera sensor to warmup
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+
+# load multi-task model
+sess = tf.InteractiveSession()
+test_model = model.ResNet_v1(sess, False)
+
+SMILE_INDEX = {0: 'Not Smile', 1: 'Smile'}
+EMOTION_INDEX = {0: 'Angry', 1: 'Disgust', 2: 'Fear', 3: 'Happy', 4: 'Sad', 5: 'Surprise', 6: 'Neutral'}
+GENDER_INDEX = {0: 'Female', 1: 'Male'}
+AGE_INDEX = {0: 'Too Young', 1: 'Young', 2: 'Old', 3: 'Too Old'}
+
+# loop over the frames from the video stream
+while True:
+	# grab the frame from the threaded video stream and resize it
+	# to have a maximum width of 400 pixels
+	frame = vs.read()
+	frame = imutils.resize(frame, width=800)
+
+	# grab the frame dimensions and convert it to a blob
+	(h, w) = frame.shape[:2]
+	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+		(300, 300), (104.0, 177.0, 123.0))
+
+	# pass the blob through the network and obtain the detections and
+	# predictions
+	net.setInput(blob)
+	detections = net.forward()
+
+	# loop over the detections
+	for i in range(0, detections.shape[2]):
+		# extract the confidence (i.e., probability) associated with the
+		# prediction
+		confidence = detections[0, 0, i, 2]
+
+		# filter out weak detections by ensuring the `confidence` is
+		# greater than the minimum confidence
+		if confidence < args["confidence"]:
+			continue
+
+		# compute the (x, y)-coordinates of the bounding box for the
+		# object
+		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+		(startX, startY, endX, endY) = box.astype("int")
+
+		#extract face ROI
+		face = frame[startY:endY, startX:endX]
+		face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+		face = cv2.resize(face, (48, 48))
+		face = (face-128.0) / 255
+		T = np.zeros([48, 48, 1])
+		T[:, :, 0] = face
+		test_img = []
+		test_img.append(T)
+		test_img = np.asarray(test_img)
+
+		feed_dict = {test_model.input_images: test_img, test_model.is_training:False, test_model.keep_prob:1.0}
+		predict_y_smile_conv = np.argmax(sess.run(test_model.y_smile_conv, feed_dict=feed_dict))
+		predict_y_emotion_conv = np.argmax(sess.run(test_model.y_emotion_conv, feed_dict=feed_dict))
+		predict_y_gender_conv = np.argmax(sess.run(test_model.y_gender_conv, feed_dict=feed_dict))
+		predict_y_age_conv = np.argmax(sess.run(test_model.y_age_conv, feed_dict=feed_dict))
+
+		smile_label = SMILE_INDEX[predict_y_smile_conv]
+		emotion_label = EMOTION_INDEX[predict_y_emotion_conv]
+		gender_label = GENDER_INDEX[predict_y_gender_conv]
+		age_label = AGE_INDEX[predict_y_age_conv]
+
+		text = "{}, {}, {}, {}".format(smile_label, emotion_label, gender_label, age_label)
+		y = startY - 10 if startY - 10 > 10 else startY + 10
+		cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255), 2)
+		cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
+
+	# show the output frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
